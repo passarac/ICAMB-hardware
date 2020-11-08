c3828ae3c7d223ad7f9c6ca9a0a0fcae89138e9c
Passara Chanchotisatien <31198485+passaraink@users.noreply.github.com>
2020-11-08 23:45:34 +0700

    Add files via upload

---
+++ hardware_code/esp32_connect_wifi/esp32_connect_wifi.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+const char* ssid     = "library";
+const char* password = "l1234567890";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  
+}
